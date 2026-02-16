7499d384172aa38e7c562308f47420d3e3a48d08
OneLoveLean <kievstar324018@gmail.com>
2026-02-17 00:02:26 +0100

    Day 2: Variables and Data Types

---
+++ day_02_variables/main.py
@@ -0,0 +1,6 @@
+Age = 25
+Waight =1.85
+Name = "Vitia"
+Is_student = True
+Is_married = False
+print(f"Hello, my name is {Name}, i am {Age} years old, my waight is {Waight} meters, I am a student: {Is_student}, I am married: {Is_married}")
