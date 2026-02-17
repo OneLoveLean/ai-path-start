6b6b69137427d9d9669abf3ea037bb50d4cea1da
OneLoveLean <kievstar324018@gmail.com>
2026-02-17 21:07:45 +0100

    Day 3: Math operations and input

---
+++ day 3/Area Calculator.py
@@ -0,0 +1,5 @@
+print("Rectangle Area Calculator")
+Length = float(input("Enter Length: "))
+Width = float(input("Enter Width: "))
+Area = Length * Width
+print(f"The area of the rectangle is: {Area}")
---
+++ day 3/Converter.py
@@ -0,0 +1,4 @@
+print("Converter from USD to PLN on 17.02.2026")
+USD = float(input("Enter the amount you want to convert from USD to PLN: "))
+PLN = USD * 3.56
+print(f"The converted amount is: {PLN} PLN")
