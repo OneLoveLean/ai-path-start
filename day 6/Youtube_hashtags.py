ea1b81370c820e51ba165dba41482429ace422f3
OneLoveLean <kievstar324018@gmail.com>
2026-02-21 00:15:22 +0100

    Day 6: Basic text preprocessing logic

---
+++ day 6/Youtube_hashtags.py
@@ -0,0 +1,7 @@
+hashtags = input("Leave some hashtags under your video! like: cats, beatifull, sun").lower()
+
+hashtags_list = hashtags.split(", ")
+
+new_hashatag = "Rock and roll"
+
+hashtags_list.append(new_hashatag)
